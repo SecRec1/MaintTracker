3fcc5f16fde5fea4e47bdccc4657eecd56dd3512
SecRec1 <Chris@securerecycling.com>
2024-08-01 11:19:01 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This web app was created by Christopher Welfley in order to track equipment and maintenance tasks.
--- src/components/pages/home.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import NavigationComponent from "../navigation-container";
-
-export class Home extends Component {
-  render() {
-    return <div></div>;
-  }
-}
